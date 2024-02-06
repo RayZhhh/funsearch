86681c17604c5219c2b9cf970d3986a29e2bcc1b
RayZhhh <hey_ray_zhang@icloud.com>
2024-02-06 23:10:49 +0800

    funsearch implementation

---
+++ README.md
@@ -0,0 +1,89 @@
+# FunSearch Implementation
+
+This repository implements the following pulication:
+
+> Romera-Paredes, B. et al. [Mathematical discoveries from program search with large language models](https://www.nature.com/articles/s41586-023-06924-6). *Nature* (2023)
+
+## Installation and Requirements
+
+Please note that **the Python version** **must larger or equal than Python 3.9**, or the '*ast*' package used in the implementations will failed to work. 
+
+You can run FunSearch for online bin packing locally if enough GPU devices are available. Or you can try to use LLM interfaces to request responses online. 
+
+Please install packages listed in *requirements.txt*.
+
+## Project Structure
+
+There are some independent directories in this project:
+
+- `bin_packing` contains heuristics discovered by FunSearch for online 1D bin packing problems, and an evaluation suite to reproduce the results reported in the paper.
+- `implementation` contains an implementation of the evolutionary algorithm, code manipulation routines, and a single-threaded implementation of the FunSearch pipeline. 
+- `llm-server` contains the implementations of  an LLM server that gets the prompt by monitoring requests from FunSearch and responses the inference results to the FunSearch algorithm. 
+
+## Files in *funsearch/implementation*
+
+There are some files in *funsearch/implementation*. They are as follows:
+
+- `code_manipulatoin.py` provides functions to modify the code in the specification.
+- `config.py` includes configs of funsearch.
+- `evaluator.py` trims the sample results from LLM, and evaluate the sampled functions.
+- `evaluator_accelerate.py` accelerates the evaluation using 'numba' library.
+- `funsearch.py` implements funsearch pipeline. 
+- `profile.py` records the score of the sampled functions.
+- `programs_database.py` evolves the sampled functions.
+- `sampler.py` sends prompts to LLM and gets results.
+
+## Run FunSearch
+
+### Parameters and Settings
+
+If you want to adjust following parameters, you should modify the code in *funsearch/implementation* manually. 
+
+- `timeout_seconds` This parameter defines the maximum evaluate time for a single function. If the evaluation time exceeds this, the evaluation process will be killed. This strategy can prevent *whille True* loop and reduce total evaluation costs, but may discard potential outstanding functions. You can modify this in *implementation/evaluator.py/class Evaluator*.
+- `_reduce_score` This function does reduction to the score of a sampled function on some instances. The reduction is implemented as *mean* by default. You can modify it in *implementation/program_database.py*, where you can find a '_reduce_score' function.
+
+### Use Local LLM
+
+First, start local LLM server.
+
+```shell
+# Suppose we are in funsearch directory (root dir of this project).
+cd llm-server
+# Start LLM server: python llm_server.py --port 8088 --path [model path] --d [GPU IDs]
+python llm_server.py --port 8088 --path /LLms/CodeLlama-34b --d 0 1 2 3 4 5
+```
+
+Then, start FunSearch.
+
+```shell
+# Run FunSearch
+python funsearch_bin_packing_local_llm.py
+```
+
+You can see logs via *Tensorboard*. Please check the *log_dir* variable defined in *bin_packing_funsearch_my_template.py*, and start the tensorboard using following instructions:
+
+```shell
+# Suppose we are in funsearch directory (root directory of this project)
+cd logs
+tensorboard --logdir funsearch_local_llm
+```
+
+### Use LLM Interfaces
+
+Start FunSearch.
+
+```shell
+# Run FunSearch
+python funsearch_bin_packing_llm_api.py
+```
+
+You can see logs via *Tensorboard*. Please check the *log_dir* variable defined in *bin_packing_funsearch_my_template.py*, and start the tensorboard using following instructions:
+
+```shell
+# Suppose we are in funsearch directory (root directory of this project).
+cd logs
+tensorboard --logdir funsearch_llm_api
+```
+
+
+
---
+++ implementation/sampler.py
@@ -0,0 +1,101 @@
+# Copyright 2023 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Class for sampling new programs."""
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+from typing import Collection, Sequence, Type
+import numpy as np
+import time
+
+from implementation import evaluator
+from implementation import programs_database
+
+
+# ======================================== LLM Source Code ========================================
+class LLM(ABC):
+    """Language model that predicts continuation of provided source code.
+    """
+
+    def __init__(self, samples_per_prompt: int) -> None:
+        self._samples_per_prompt = samples_per_prompt
+
+    def _draw_sample(self, prompt: str) -> str:
+        """Returns a predicted continuation of `prompt`."""
+        raise NotImplementedError('Must provide a language model.')
+
+    @abstractmethod
+    def draw_samples(self, prompt: str) -> Collection[str]:
+        """Returns multiple predicted continuations of `prompt`."""
+        return [self._draw_sample(prompt) for _ in range(self._samples_per_prompt)]
+
+
+# =================================================================================================
+
+
+class Sampler:
+    """Node that samples program continuations and sends them for analysis.
+    """
+    _global_samples_nums: int = 0  # RZ: this variable records the global sample nums
+
+    def __init__(
+            self,
+            database: programs_database.ProgramsDatabase,
+            evaluators: Sequence[evaluator.Evaluator],
+            samples_per_prompt: int,
+            max_sample_nums: int | None = None,
+            llm_class: Type[LLM] = LLM
+    ):
+        self._samples_per_prompt = samples_per_prompt
+        self._database = database
+        self._evaluators = evaluators
+        self._llm = llm_class(samples_per_prompt)
+        self._max_sample_nums = max_sample_nums
+
+    def sample(self, **kwargs):
+        """Continuously gets prompts, samples programs, sends them for analysis.
+        """
+        while True:
+            # stop the search process if hit global max sample nums
+            if self._max_sample_nums and self._samples_per_prompt > self._max_sample_nums:
+                break
+
+            prompt = self._database.get_prompt()
+            reset_time = time.time()
+            samples = self._llm.draw_samples(prompt.code)
+            sample_time = (time.time() - reset_time) / self._samples_per_prompt
+            # This loop can be executed in parallel on remote evaluator machines.
+            for sample in samples:
+                self._global_sample_nums_plus_one()  # RZ: add _global_sample_nums
+                cur_global_sample_nums = self._get_global_sample_nums()
+                chosen_evaluator: evaluator.Evaluator = np.random.choice(self._evaluators)
+                chosen_evaluator.analyse(
+                    sample,
+                    prompt.island_id,
+                    prompt.version_generated,
+                    **kwargs,
+                    global_sample_nums=cur_global_sample_nums,
+                    sample_time=sample_time
+                )
+
+    def _get_global_sample_nums(self) -> int:
+        return self.__class__._global_samples_nums
+
+    def set_global_sample_nums(self, num):
+        self.__class__._global_samples_nums = num
+
+    def _global_sample_nums_plus_one(self):
+        self.__class__._global_samples_nums += 1
